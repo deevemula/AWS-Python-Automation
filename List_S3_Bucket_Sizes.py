f091a777b3ee1667352172ef377afa97505719ac
Maheshkrishna Gopal <maheshkrishnagopal@gmail.com>
2020-05-06 07:10:28 -0700

    Updated the environment variables to profile

--- List_S3_Bucket_Sizes.py
+++ List_S3_Bucket_Sizes.py
@@ -2,18 +2,21 @@ from __future__ import division
 import boto3
 import os
 
-client = boto3.client('s3', aws_access_key_id=os.environ['AWS_SECRET_KEY'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], region_name = os.environ['AWS_REGION'])
+session = boto3.Session(profile_name='default')
+client = boto3.client('s3')
 
 response = client.list_buckets()
 
 
 def get_size(bucket_name):
     size = 0
-    res = boto3.resource('s3', aws_access_key_id=os.environ['AWS_SECRET_KEY'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], region_name= os.environ['AWS_REGION'])
+    res = boto3.resource('s3')
     data = res.Bucket(bucket_name)
     for obj in data.objects.all():
         size += obj.size
-    print("The Bucket '{}' in {} region, occupies '{}' bytes or '{}' MB.".format(bucket_name,os.environ['AWS_REGION'],size, size/(1024*1024)))
+    print("The Bucket '{}' in {} region, occupies '{}' bytes or '{}' MB.".format(bucket_name, session.region_name, size, size/(1024*1024)))
+
+
 
 for rec in response['Buckets']:
     get_size(rec['Name'])
